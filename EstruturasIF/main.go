2c8c6af730511db7b8c081a53673af41618c0280
Eduardo da Silva Sabino <eduardodasabino@gmail.com>
2021-10-13 01:26:26 -0300

    Alteração

--- EstruturasIF/main.go
+++ EstruturasIF/main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Print("Digite um numero: ")
 	fmt.Scanf("%d", &numero)
 	if numero <= 5 {
-		fmt.Println("Numermo menor que 5e menor que 10")
+		fmt.Println("Numero menor que 5")
 	} else if numero < 10 {
 		fmt.Println("numero menor que 10 e maior que 5")
 	} else {
